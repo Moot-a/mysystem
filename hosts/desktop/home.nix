41539a326b1b19d8c7c07e00356f4fa1c545b93d
Moota <moota.moota@proton.me>
2025-12-10 19:55:10 +0100

    Added state version

---
+++ hosts/desktop/home.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  home.stateVersion = "24.11";
+  # keep whatever is already there, and add:
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      alias ns="sudo nixos-rebuild switch --flake ~/mysystem#moota-desktop"
+    '';
+  };
+}
